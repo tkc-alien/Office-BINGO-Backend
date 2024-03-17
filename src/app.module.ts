5f69f275f6846f4b85ca6b10ad0ea44681e1e663
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-03-17 09:57:06 +0900

    add: DB接続モジュールを追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "@/app.controller";
 import { AppService } from "@/app.service";
+import {
+  LotteryEntity,
+  SheetEntity,
+  SheetSquareEntity,
+  UserEntity,
+} from "@/entity";
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      entities: [LotteryEntity, SheetSquareEntity, SheetEntity, UserEntity],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
