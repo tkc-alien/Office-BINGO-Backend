1a791536a172da01e635bfb7d04c3bd770b53381
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-03-22 00:04:43 +0900

    add: Firebase初期化処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from "@nestjs/core";
+import admin from "firebase-admin";
 
 import { AppModule } from "@/app.module";
 
 async function bootstrap() {
+  // Firebase初期化
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+
+  // App初期化
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
