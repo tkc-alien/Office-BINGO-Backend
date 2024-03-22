167bd99141e708c0fbb3bb75ffcb4b3321d28dce
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-03-22 20:55:15 +0900

    add: 共通レスポンス対応したSwaggerデコレータを作成

---
+++ src/decorator/api-array-response.decorator.ts
@@ -0,0 +1,38 @@
+import { applyDecorators, Type } from "@nestjs/common";
+import { ApiExtraModels, ApiResponse, getSchemaPath } from "@nestjs/swagger";
+
+type Options<T extends Type> = {
+  type: T;
+  status?: number;
+};
+
+/**
+ * 共通レスポンスに合わせたSwaggerデコレータ
+ * @param statusCode
+ * @param model
+ * @returns
+ */
+export const ApiArrayResponse = <T extends Type>(options: Options<T>) => {
+  // デコレータ作成
+  return applyDecorators(
+    ApiExtraModels(Response, options.type),
+    ApiResponse({
+      status: options.status ?? 200,
+      schema: {
+        allOf: [
+          { $ref: getSchemaPath(Response) },
+          {
+            properties: {
+              data: {
+                type: "array",
+                items: {
+                  $ref: getSchemaPath(options.type),
+                },
+              },
+            },
+          },
+        ],
+      },
+    })
+  );
+};
