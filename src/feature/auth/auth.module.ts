1db14e7d68fe3aab7162fff23bf5de6e3326f879
kouhei-takashima <kouhei-takashima@dip-net.co.jp>
2024-03-22 20:56:58 +0900

    add: AuthモジュールにUserモジュールをインポート

--- src/feature/auth/auth.module.ts
+++ src/feature/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { PassportModule } from "@nestjs/passport";
 import { AuthService } from "@/feature/auth/auth.service";
 import { UidAuthStrategy } from "@/feature/auth/uid-auth.strategy";
 import { UserAuthStrategy } from "@/feature/auth/user-auth.strategy";
+import { UserModule } from "@/feature/user/user.module";
 
 @Module({
-  imports: [PassportModule],
+  imports: [PassportModule, UserModule],
   providers: [AuthService, UidAuthStrategy, UserAuthStrategy],
 })
 export class AuthModule {}
